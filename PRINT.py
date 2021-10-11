82e82660ceeb3a57ca457f028511660a02c01b4f
raja <rdas828726@gmail.com>
2021-10-11 12:29:49 +0530

    #5

---
+++ PRINT.py
@@ -0,0 +1,18 @@
+#for one line comment we use #
+"""and
+for
+multiline comment
+we use """
+
+
+print("hello world")
+#for new line
+print("for new line")
+#for same line
+print("(1)this is for same line", end=" ")
+print("(2)for same line")
+#also we can use for same line
+print("(1)this is another line","(2)this is second line")
+# \n is use for new line charrcter this is escape sequence character
+
+
