53ef65399b199287b66a1b5f4c8b7120af4c89dc
raja <rdas828726@gmail.com>
2021-10-11 12:29:16 +0530

    #3

---
+++ IF&ELSE.py
@@ -0,0 +1,21 @@
+var1=3
+var2=5
+var3=6
+"""cheaking var3 is greater then var1 if 
+false condition it will go to elif or if it is ture then it will print"""
+if var3>var1:
+    print(var3,": is GREATER")
+elif var3>var2:
+    print(var3,": is greater")
+elif var2>var1:
+    print(var2,":is greater")
+else :
+    print(var1,":is greater")
+
+list1=[1,3,26,4,6,7]
+# for cheaking element in list function
+if 7 in list1:#only for element checking(if it's ture)
+    print(7,"it is present in list")
+if 5 not in list1:#if true o/p =not in list or flase o/p= ______
+    print(5,"not in list")
+
