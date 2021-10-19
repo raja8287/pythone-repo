71c5b6640467113dc2506fd81c988c6631b47485
raja <rdas828726@gmail.com>
2021-10-19 22:46:52 +0530

    advanced file functions

--- file_wrighting.py
+++ file_wrighting.py
@@ -1,9 +1,17 @@
-f=open("raja.txt","w")#will erase all the content of the .txt file and overwright f.wright("word")
+#f=open("raja.txt","w")#will erase all the content of the .txt file and overwright f.wright("word")
 
-a=f.write("wright words")
-print(a)
-f.close()
+#a=f.write("wright words")
+#print(a)
+#f.close()
 #in append mode
 p=open("raja.txt","r+")
 print(p.read())
-p.write(" thank u")
+#p.write(" thank u")
+print(p.readline())
+print(p.tell())
+p.close()
+#another way to open a file
+with open("raja.txt") as f:
+    a=f.readline()
+    print(a)
+    #no need to close the file
